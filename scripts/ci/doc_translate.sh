c63b791ef3fb64ce09f8885d4c0c605e89a6a688
Danilo G. Baio (dbaio) <dbaio@bsd.com.br>
2019-12-30 21:30:32 -0300

    ci/doc_translate.sh: Check for .do_not_translate flag

--- scripts/ci/doc_translate.sh
+++ scripts/ci/doc_translate.sh
@@ -18,12 +18,18 @@ fi
 
 for article in $(find "$1/articles/" -type d -maxdepth 1 -mindepth 1); do
 	cd "$article"
+	if [ -f ".do_not_translate" ]; then
+		continue
+	fi
 	make tran
 	cd -
 done
 
 for book in $(find "$1/books/" -type d -maxdepth 1 -mindepth 1); do
 	cd "$book"
+	if [ -f ".do_not_translate" ]; then
+		continue
+	fi
 	make tran
 	cd -
 done
